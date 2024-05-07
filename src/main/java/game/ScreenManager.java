3dd272e11ce714f1564b018c852761ab804be075
Jay910066 <jay07291@gmail.com>
2024-05-08 01:15:02 +0800

    Change the logic of switch screen

--- src/main/java/game/ScreenManager.java
+++ src/main/java/game/ScreenManager.java
@@ -1,14 +1,23 @@
 package game;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ScreenManager {
     private Stage stage;
+    private VBox root = new VBox();
+    private Scene scene = new Scene(root);
 
     public ScreenManager(Stage stage) {
         this.stage = stage;
+        root.setAlignment(Pos.CENTER);
+        stage.setScene(scene);
         stage.setFullScreenExitHint("");
         stage.setFullScreen(true);
         stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
@@ -16,22 +25,19 @@ public class ScreenManager {
 
     public void switchToMainMenu() {
         MainMenu mainMenu = new MainMenu(this);
-        Scene scene = new Scene(mainMenu);
-        stage.setScene(scene);
-        stage.setFullScreen(true);
+        root.getChildren().clear();
+        root.getChildren().add(mainMenu);
     }
 
     public void switchToSettings(String previousScreen) {
         Settings settings = new Settings(this, previousScreen);
-        Scene scene = new Scene(settings);
-        stage.setScene(scene);
-        stage.setFullScreen(true);
+        root.getChildren().clear();
+        root.getChildren().add(settings);
     }
 
     public void switchToSongListMenu() {
         SongListMenu songListMenu = new SongListMenu(this);
-        Scene scene = new Scene(songListMenu);
-        stage.setScene(scene);
-        stage.setFullScreen(true);
+        root.getChildren().clear();
+        root.getChildren().add(songListMenu);
     }
 }
