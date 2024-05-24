adf13024ba946ea589cd9de51b4cb0ae07b3cc5a
Inori1208 <x65124x@gmail.com>
2024-05-24 13:59:29 +0800

    Local config file can be edited by settings.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module game.musicgame_project {
     requires com.almasb.fxgl.all;
     requires javafx.media;
     requires java.desktop;
+    requires com.fasterxml.jackson.databind;
 
     opens game to javafx.fxml;
     exports game;
